4a891843bddc7b662b442501bb77f5cae307dee6
niccolot <nico_tiezzi98@hotmail.it>
2023-02-21 19:43:20 +0100

    done all prepro functs

--- preprocessing.py
+++ preprocessing.py
@@ -59,14 +59,6 @@ def get_char_info(sentences):
     return sentences
 
 
-def get_dataset(file_path):
-
-    sentences, labels = get_lines(file_path)
-    sentences_charinfo = get_char_info(sentences)
-
-    return sentences_charinfo, labels
-
-
 def get_casing(word, case_lookup):
     """
     given a dictionary-type lookup table that maps the different casing of a word
@@ -110,14 +102,17 @@ def get_embeddings(sentences, labels, word_embedding_file):
     integer indices and 2 lists containing the word_embeddings (given by the file) and the
     case_embeddings (given by a lookup table)
     """
+
     label_set = set()
     words = {}
 
     # save unique words and labels in the dataset
-    for i in range(len(sentences)):
-        for token, char, label in zip(sentences[i], labels[i]):
-            label_set.add(label)
+    for sentence, sentence_labels in zip(sentences, labels):
+        for token, char in sentence:
+            #label_set.add(label)
             words[token.lower()] = True
+        for label in sentence_labels:
+            label_set.add(label)
 
     # dict mapping labels to integer indices
     label2idx = {}
@@ -173,26 +168,42 @@ def get_embeddings(sentences, labels, word_embedding_file):
 
     word_embeddings = np.array(word_embeddings)
 
-    return word2idx, case2idx, char2idx, word_embeddings, case_embeddings
+    return word2idx, case2idx, char2idx, label2idx, word_embeddings, case_embeddings
 
 
-def create_embedding_matrices(sentences, word2idx, label2idx, case2idx, char2idx):
+def create_integer_embedding(sentences, labels,  word2idx, label2idx, case2idx, char2idx):
+    """
+    given sentences and labels of the dataset (as strings) and the mapping dictionaries
+    it returns a list that embeds the datasets as integers in order to be fed to the
+    network's embedding layer, which will output embedding vectors of real numbers
 
-    unknown_idx = word2idx['UNKNOWN_TOKEN']
-    padding_idx = word2idx['PADDING_TOKEN']
+    :param sentences: (list) list of lists with sentences and character-level info
+    :param labels: (list) list of lists with the labels paired with each word in a sentence
+    :param word2idx: (dict) dict mapping words to integer indices obtained from the vocabulary of the
+    pretrained word embedding
+    :param label2idx: (dict) dictionary mapping labels to integer indices
+    :param case2idx: (dict) dictionary mapping casing info to integer indices
+    :param char2idx: (dict) dictionary mapping character info to integer indices
+    :return: (lsit, list, int) the list containing the dataset and labels embedded as integers
+    and the number of words in the dataset that were not in the embedding vocabulary and thus
+    embedded as 'unknown' words
+    """
 
-    dataset = []
+    unknown_idx = word2idx['UNKNOWN']
+
+    dataset_embedding = []
+    labels_embedding = []
 
     word_count = 0
     unknown_word_count = 0
 
-    for sentence in sentences:
+    for sentence, sentence_labels in zip(sentences, labels):
         word_indices = []
         case_indices = []
         char_indices = []
         label_indices = []
 
-        for word, char, label in sentence:
+        for word, char in sentence:
             word_count += 1
             if word in word2idx:
                 word_idx = word2idx[word]
@@ -204,12 +215,37 @@ def create_embedding_matrices(sentences, word2idx, label2idx, case2idx, char2idx
             char_idx = []
             for x in char:
                 char_idx.append(char2idx[x])
-            # Get the label and map to int
+
             word_indices.append(word_idx)
             case_indices.append(get_casing(word, case2idx))
             char_indices.append(char_idx)
+
+        for label in sentence_labels:
             label_indices.append(label2idx[label])
 
-        dataset.append([word_indices, case_indices, char_indices, label_indices])
+        dataset_embedding.append([word_indices, case_indices, char_indices])
+        labels_embedding.append(label_indices)
 
-    return dataset
+    return dataset_embedding, labels_embedding, unknown_word_count
+
+
+def padding(embedded_dataset):
+    """
+    pads the character level integer embedding with zeros as the CNN for character-level
+    embeddings will take as input fixed sized sequences of characters.
+    plotting the frequency of the lengths of all the sentences as a histogram is clear that
+    the great majority of the sentences contain ~50 chars maximum so the 0-padding is set to 50,
+    with a minimal loss of information and resulting in a smaller model (instead of padding
+    all sentences to be as long as the longest in the dataset which is ~130 chars)
+
+    :param embedded_dataset: (list) integer-embedded dataset
+    :return: (list) integer-embedded dataset with the chars 0-padded to be 50 for all the sentences
+    """
+
+    max_len = 50
+    for i, sentence in enumerate(embedded_dataset):
+        embedded_dataset[i][2] = tf.keras.utils.pad_sequences(embedded_dataset[i][2],
+                                                              maxlen=max_len,
+                                                              padding='post')
+    return embedded_dataset
+
